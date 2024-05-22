b372cfc820b24e77964623f66744aca63271ed2b
nina <annisamustofa@outlook.com>
2024-05-22 09:41:24 +0700

    adjusted height for banners in about us and products

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -6,7 +6,7 @@ import Culture from "./components/culture";
 const AboutUs: React.FC = () => {
   return (
     <div className="w-full bg-ragewhite">
-      <div className="banner inset-0 mt-navheight box-border h-[720px] overflow-hidden bg-[url('/banner/about-banner.webp')] bg-cover bg-center"></div>
+      <div className="banner inset-0 box-border h-[980px] overflow-hidden bg-[url('/banner/about-banner.webp')] bg-cover bg-center"></div>
 
       <AboutText />
       <TeamSum />
---
+++ components/signin-form/form.tsx
@@ -0,0 +1,24 @@
+// import React from "react";
+// import { useFormik, Formik } from "formik";
+
+// const SignInForm: React.FC = () => {
+//   const formik = useFormik({
+//     initialValues: {
+//       username: "",
+//       password: "",
+//     },
+//     onSubmit: (values) => {
+//       alert(JSON.stringify(values, null,)));
+//     },
+//   });
+
+//   return (
+//     <form action="submit" onSubmit={formik.handleSubmit} autoComplete="off">
+//       <label htmlFor="name">Name</label>
+//       <input type="text" placeholder="Name" />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// };
+
+// export default SignInForm;
